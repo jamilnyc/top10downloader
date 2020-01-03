a1b8c996c72f1dab680116e93ea171762421aaa8
Murshed J. Ahmed <mahmed@marvel.com>
2020-01-02 22:38:04 -0500

    Created inner class for asynchronous tasks

--- app/src/main/java/academy/learnprogramming/top10downloader/MainActivity.kt
+++ app/src/main/java/academy/learnprogramming/top10downloader/MainActivity.kt
@@ -1,14 +1,38 @@
 package academy.learnprogramming.top10downloader
 
+import android.os.AsyncTask
 import android.os.Bundle
+import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
+    private val TAG = "MainActivity"
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        Log.d(TAG, "onCreate() called")
+        val downloadData = DownloadData()
+        downloadData.execute("URL goes here")
+        Log.d(TAG, "onCreate(): done")
+    }
 
+    // First parameter is the String URL
+    // Second parameter is for a progress bar, but we are not using it, so Void
+    // Third parameter is the string result (XML)
+    companion object {
+        private class DownloadData : AsyncTask<String, Void, String>() {
+            private val TAG = "DownloadData"
 
+            override fun onPostExecute(result: String?) {
+                super.onPostExecute(result)
+                Log.d(TAG, "onPostExecute(): parameter is $result")
+            }
+
+            override fun doInBackground(vararg params: String?): String {
+                Log.d(TAG, "doInBackground(): starts with ${params[0]}")
+                return "doInBackground completed"
+            }
+        }
     }
 }
